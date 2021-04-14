51394954cc5bb90f876de68a43cecfc6b9aa922c
Robert Porter <robertp.dev@gmail.com>
2021-04-15 00:50:22 +0900

    Simplifying test

--- tests/stream_test.py
+++ tests/stream_test.py
@@ -3,6 +3,7 @@ import src.spacemaps.stream as stream
 
 TEST_FILE = 'https://amp-spacemaps-technical-challenge.s3-ap-northeast-1.amazonaws.com/spacemaps_technical_challenge.txt'
 
+
 class TestStream(unittest.TestCase):
     def test_to_row(self):
         a = stream.to_row(b'someid 44')
@@ -10,7 +11,8 @@ class TestStream(unittest.TestCase):
 
     def test_request_stream(self):
         with stream.create_stream(TEST_FILE) as stream_:
-            self.assertTrue(all([True for _ in stream_]))
+            first = next(stream_)
+            self.assertTrue(first.id == '1a68e13af0f21d62bdd', first.value == 2000)
 
     def test_mapped_iterator(self):
         input = [1, 2, 3, 4]
